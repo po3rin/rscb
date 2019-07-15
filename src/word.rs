2c993f569b76e90b70afe40c812a6410caa7e0dc
po3rin <abctail30@gmail.com>
2019-07-16 02:23:09 +0900

    adjust usize

--- src/word.rs
+++ src/word.rs
@@ -14,8 +14,11 @@ pub fn words(s: String) {
         }
         id_to_word.insert(i, item);
         word_to_id.insert(item, i);
-        corpus.push(i);
-        i = i + 1
+        i += 1;
+    }
+
+    for item in s.split_whitespace() {
+        corpus.push(word_to_id[item]);
     }
 
     println!("{:#?}", &id_to_word);
@@ -23,23 +26,22 @@ pub fn words(s: String) {
     println!("{:#?}", &corpus);
 
     let corpus_len = corpus.len();
-    let vocab_len = corpus.len();
+    let vocab_len = word_to_id.len();
     let window_size = 1;
 
     let mut a = Array2::<i64>::zeros((vocab_len, vocab_len));
-    println!("{:#?}", a);
 
     for word_id in &corpus {
-        let left_id = word_id - 1;
-        let right_id = word_id + 1;
-        if left_id > 0 {
-            let left_word_id = corpus[left_id as usize];
-            a[[2, 3]] += 1;
+        if *word_id > 1 {
+            let left_id = word_id - 1;
+            let left_word_id = corpus[left_id];
+            a[[*word_id, left_word_id]] += 1;
         }
 
-        if right_id < vocab_len as i32 {
-            let right_word_id = corpus[right_id as usize];
-            a[[2, 2]] += 1;
+        if *word_id < (corpus_len - 1) {
+            let right_id = word_id + 1;
+            let right_word_id = corpus[right_id];
+            a[[*word_id, right_word_id]] += 1;
         }
     }
     println!("{:#?}", a);
